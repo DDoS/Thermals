a0c4dd2e070e61bf2918645e57998bd16cf8ff74
Aleksi Sapon <alek42008@yahoo.com>
2013-05-03 22:33:09 -0400

    Stub commands

--- src/main/java/me/ddos/thermals/command/ThermalsCommands.java
+++ src/main/java/me/ddos/thermals/command/ThermalsCommands.java
@@ -1,6 +1,5 @@
 package me.ddos.thermals.command;
 
-import me.ddos.thermals.ThermalsPlugin;
 import me.ddos.thermals.command.CommandHandler.CommandMethod;
 import me.ddos.thermals.command.CommandHandler.RequiredValues;
 import me.ddos.thermals.data.IntLocation;
@@ -12,14 +11,52 @@ import org.bukkit.command.CommandSender;
  */
 public class ThermalsCommands {
 	@CommandMethod
-	@RequiredValues({"set"})
-	public void setLocation(CommandSender sender, String subCommand, IntLocation location, Integer size) {
-		ThermalsPlugin.logInfo("SET Success!: " + subCommand + " " + location + " " + size);
+	@RequiredValues({"clear", "all"})
+	public void clearAll(CommandSender sender, String subCommand1, String subCommand2) {
+	}
+
+	@CommandMethod
+	@RequiredValues({"clear"})
+	public void clearLocation(CommandSender sender, String subCommand1, IntLocation location) {
+	}
+
+	@CommandMethod
+	@RequiredValues({"clear"})
+	public void clearArea(CommandSender sender, String subCommand1, IntLocation from, IntLocation to) {
+	}
+
+	@CommandMethod
+	@RequiredValues({"clear"})
+	public void clearArea(CommandSender sender, String subCommand1, IntLocation location, Integer size) {
 	}
 
 	@CommandMethod
 	@RequiredValues({"get"})
-	public void getLocation(CommandSender sender, String subCommand, IntLocation location, Integer size) {
-		ThermalsPlugin.logInfo("GET Success!: " + subCommand + " " + location);
+	public void getLocation(CommandSender sender, String subCommand, IntLocation location) {
+	}
+
+	@CommandMethod
+	@RequiredValues({"set"})
+	public void setLocation(CommandSender sender, String subCommand, IntLocation location, Integer heat) {
+	}
+
+	@CommandMethod
+	@RequiredValues({"set"})
+	public void setArea(CommandSender sender, String subCommand, IntLocation from, IntLocation to, Integer heat) {
+	}
+
+	@CommandMethod
+	@RequiredValues({"set"})
+	public void setArea(CommandSender sender, String subCommand, IntLocation from, Integer size, Integer heat) {
+	}
+
+	@CommandMethod
+	@RequiredValues({"gen"})
+	public void generateArea(CommandSender sender, String subCommand, IntLocation from, IntLocation to, String fileName) {
+	}
+
+	@CommandMethod
+	@RequiredValues({"gen"})
+	public void generateArea(CommandSender sender, String subCommand, IntLocation from, Integer size, String fileName) {
 	}
 }
